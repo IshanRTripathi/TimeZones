d89ad06ea4ec2d75021ada7e9985e832dcb2ccaa
ishanrtripathi <ishanrtripathi786@gmail.com>
2021-06-16 00:36:51 +0530

    Complete implementation of timezone

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-let moment= require('moment');
+const moment= require('moment-timezone');
 
-console.log(moment().format('dddd'));
+moment.tz.setDefault('India/Kolkata');
+
+let targetTimezone= 'Europe/Paris';
+
+if(process.argv.length <3){
+    console.log("Enter a timezone !");
+    process.exit(1);
+} else {
+    targetTimezone= process.argv[2];
+}
+
+// console.log(moment().format('dddd'));
+
+console.log(`Time of timezone ${targetTimezone} is ${moment().tz(targetTimezone).format()}`);
